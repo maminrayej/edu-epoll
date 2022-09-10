32f419bef7952dc9d3a6959856676bcb18951222
M.Amin Rayej <m.amin.rayej@gmail.com>
2022-09-10 22:13:07 +0430

    barebone implementation of core components

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,45 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use std::os::unix::prelude::{AsRawFd, FromRawFd, OwnedFd};
+
+use bitflags::bitflags;
+
+macro_rules! syscall {
+    ($fn: ident ( $($arg: expr),* )) => {{
+        #[allow(unused_unsafe)]
+        let result = unsafe { libc::$fn($($arg, )*) };
+
+        if result == -1 {
+            Err(std::io::Error::last_os_error())
+        } else {
+            Ok(result)
+        }
+    }};
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+bitflags! {
+    pub struct Interest: u32 {
+        const READ = libc::EPOLLIN as u32;
+        const WRITE = libc::EPOLLOUT as u32;
     }
+}
+
+pub struct Poll {
+    registry: Registry,
+}
+
+pub struct Events {
+    inner: Vec<libc::epoll_event>,
+}
+
+impl Poll {
+    pub fn new() -> Result<Self, std::io::Error> {
+        let fd = unsafe { OwnedFd::from_raw_fd(syscall!(epoll_create1(libc::O_CLOEXEC))?) };
+
+        Ok(Poll {
+            registry: Registry { fd },
+        })
+    }
+}
+
+pub struct Registry {
+    fd: OwnedFd,
 }
